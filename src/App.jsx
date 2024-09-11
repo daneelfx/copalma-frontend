1e4dbae6b59f8ea14d8643044a58a4a4b717f843
Daniel Felipe <daneelfx@gmail.com>
2024-09-11 00:40:49 -0500

    add react router to project

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ import Header from './components/Header'
 import '@mantine/core/styles.css'
 import '@mantine/dates/styles.css'
 
-import { MantineProvider } from '@mantine/core'
-
 function App() {
   return (
     <MantineProvider>
-      {/* <Header /> */}
       <PollinationMap />
     </MantineProvider>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import { Link } from 'react-router-dom'
+import { MantineProvider } from '@mantine/core'
+import PollinationMap from './pages/PollinationMap'
+
+import Header from './components/Header'
+import Login from './pages/Login.jsx'
+
+import '@mantine/core/styles.css'
+import '@mantine/dates/styles.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/map',
+    element: (
+      <MantineProvider>
+        <PollinationMap />
+      </MantineProvider>
+    ),
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 )
